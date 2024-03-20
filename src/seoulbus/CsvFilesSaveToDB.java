94ef0d3a6ff644569c635d2c720463faf73a9911
dlsgp <khyioh4413@gmail.com>
2024-03-20 17:19:16 +0900

    first commit

--- src/seoulbus/CsvFilesSaveToDB.java
+++ src/seoulbus/CsvFilesSaveToDB.java
@@ -1,13 +1,18 @@
 package seoulbus;
 
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class CsvFilesSaveToDB {
-    private final static String filePath = "2021_seoul_bus_onoff.csv";
-    public static void main(String[] args) {
-        final String jdbcURL = "jdbc:mysql://loccalhost:3306/yscom";
+    private final static String filePath = "C:/java-sql/2021_seoul_bus_onoff.csv";
+    public static <CSVRecord> void main(String[] args) {
+        final String jdbcURL = "jdbc:mysql://localhost:3306/yscom";
         final String username = "root";
         final String password = "1234";
 
@@ -26,6 +31,51 @@ public class CsvFilesSaveToDB {
 //            System.out.println(e.getMessage());
 //        }
         System.out.println("연결됨");
+
+        StringBuffer sb = new StringBuffer("null");
+
+        for(int i = 1; i<=55; i++)
+            sb.append(",? ");
+        sb.setLength((sb.length()-1));
+
+        String sql = "insert into tbl_seoul_bus_onoff values (" + sb.toString() + ");";
+        System.out.println(sql);
+
+        PreparedStatement statement = null;
+        try{
+            statement = connection.prepareStatement(sql);
+        } catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+        int columSize = 55;
+
+        List<CSVRecord> records = null;
+        try{
+            records = getCsvRecords();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        for(int row = 0; row<records.size(); row++){
+            CSVRecord data = records.get(row);
+            for(int fieldIndex = 0; fieldIndex < columSize; fieldIndex ++ ){
+                try{
+                    statement.setString(fieldIndex + 1 );
+                }
+            }
+        }
+
+
     }
 
+    public static List <CSVRecord> getCsvRecords() throws I0Exception {
+        File targetFile = new File(filePath);
+        int sampleDateRow = 0;
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(targetFile)){
+            List <CSVRecord> records;
+            try(CSVParser parser = CSVFormat.EXCEL.withFirstRecordAsHeader().withQuote('"').parchase(bufferedReader)){
+            records = parser.getRecords();
+            }
+        }
+        return null;
+    }
 }
